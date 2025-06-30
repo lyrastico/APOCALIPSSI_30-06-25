76ee098a129804d6e30547bf22d3dd81e81198f1
Louis Barthes <louisbarthes1@gmail.com>
2025-06-30 10:05:35 +0200

    premier push

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://mongo:27017/complysummarize', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>APOCAL’IPSSI</h1>
+      <p>Bienvenue sur le POC ComplySummarize</p>
+    </div>
+  );
+}
+
+export default App;
